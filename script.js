c8a610575ca5ba36f0636ec957fa2bff040837da
SaidmuhammadAkromov <saidakromov2002@gmail.com>
2021-04-18 15:15:48 +0500

    test edit

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function showModal() {
+function showModal(block) {
     var modal = document.getElementById('modal')
     modal.style.display = 'block'
 }
-function closeModal() {
+function closeModal(block) {
     var modal = document.getElementById('modal')
     modal.style.display = 'none'
+    block.style.display = 'none'
 }
 function handleClickAdd() {
     var nameInput = document.getElementById('nameInput')
@@ -22,9 +23,9 @@ function handleClickAdd() {
     // cardAddress.classList.add('btn-primary')
     // cardAddress.innerText = marker._latlng.lat + marker._latlng.lng
     // cardBody.append(cardAddress)
-    createCard(nameInput.value, costInput.value, URL.createObjectURL(file), )
-    createObj(nameInput.value, costInput.value, URL.createObjectURL(file))
-    var card = createObj(nameInput.value, costInput.value, URL.createObjectURL(file))
+    createCard(nameInput.value, costInput.value, URL.createObjectURL(file), marker._latlng + " " + marker._latlng.lng)
+    createObj(nameInput.value, costInput.value, URL.createObjectURL(file), marker._latlng + " " + marker._latlng.lng)
+    var card = createObj(nameInput.value, costInput.value, URL.createObjectURL(file), marker._latlng.lat + " " + marker._latlng.lng)
     writeToLS(card)
 }
 function createObj(title, cost, imageSrc, address) {
@@ -78,18 +79,30 @@ function createCard(title, cost, imageSrc, address) {
     cardBody.append(cardCost)
 
     var cardAddress = document.createElement('a')
-    cardAddress.classList.add('btn-primary')
+    cardAddress.classList.add('btn-primary', 'btn')
     cardAddress.innerText = address
     cardBody.append(cardAddress)
+
+    var editBtn = document.createElement('button')
+    editBtn.classList.add('btn', 'btn-secondary', 'editBtn')
+    editBtn.innerText = 'Edit'
+    cardBody.append(editBtn)
 }
 function createCardOfLS() {
     var items = JSON.parse(localStorage.getItem('items'))
     for (let index = 0; index < items.length; index++) {
         const item = items[index];
-        createCard(item.title, item.cost, item.imageSrc)
+        createCard(item.title, item.cost, item.imageSrc, item.address)
     }
 }
-function initMap() {
+function click(e) {
+    var lat1 = e.latlng.lat;
+    var lng1 = e.latlng.lng;
+
+
+    marker.setLatLng([lat1, lng1])
+}
+
     var mymap = L.map('mymap').setView([41.311081, 69.240562], 13);
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic2FpZG11aGFtbWFkYWtyb21vdiIsImEiOiJja25hZjBudnAxaDQ4MnhwOXE4dmZ6bjVzIn0.ppLFtGB0JSKeX8gUqLyAVw', {
@@ -101,14 +114,37 @@ function initMap() {
         accessToken: 'your.mapbox.access.token'
     }).addTo(mymap);
     var marker = L.marker([41.311081, 69.240562]).addTo(mymap)
+    
+function showEditModal() {
+    var editModal = document.getElementById('editModal')
+    editModal.style.display = 'block'
+}    
 
-    return mymap;
+function getElementsToEditModal(editbutton) {
+    var parentElement = editbutton.parentElement
+    var editTitleInput = document.getElementById('editNameInput')
+    var editCostInput = document.getElementById('editCostInput')
+    
+
+    editTitleInput.value = parentElement.firstElementChild.innerText
+    editCostInput.value  = parentElement.children[1].innerText
 }
+
+    var showEditModalBtns = document.getElementsByClassName('editBtn')
+    for (let index = 0; index < showEditModalBtns.index; index++) {
+        const element = showEditModalBtns[index];
+        // element.addEventListener('click', function () {
+        //     showEditModal()
+        //     getElementsToEditModal(this)
+        // })
+        console.log(element);
+    }
+
+
 window.addEventListener('load', function () {
     var showModalBtn = document.getElementById('showModalBtn')
     showModalBtn.addEventListener('click', function () {
         showModal()
-        initMap()
     })
     var closeModalBtn = document.getElementById('closeModalBtn')
     closeModalBtn.addEventListener('click', function () {
@@ -127,9 +163,22 @@ window.addEventListener('load', function () {
     var choosePhotoBtn = document.getElementById('choosePhoto')
     choosePhotoBtn.addEventListener('click', function(){
         document.getElementById('fileInput').click()
-    }) 
+    })
 
-    createCardOfLS()
+    mymap.on('click', click);
+
+    createCardOfLS();
+
+    var editCloseModalBtnX = document.getElementById('editCloseModalBtnX')
+    editCloseModalBtnX.addEventListener('click', function () {
+        closeModal(document.getElementById('editModal'))
+    })
+
+    var editCloseModalBtn = document.getElementById('editCloseModalBtn')
+    editCloseModalBtn.addEventListener('click', function () {
+        closeModal(document.getElementById('editModal'))
+    })
+
 
 
 })
